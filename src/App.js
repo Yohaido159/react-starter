4669bc72be2fad6ac005c86a8720f5625a143541
Yohaido159 <Yohaido159@gmail.com>
2022-04-02 21:49:45 +0300

    add change

--- src/App.js
+++ src/App.js
@@ -3,10 +3,7 @@ import { Routes, Route, Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
-import Dashboard from "./pages/Dashboard/Dashboard";
 import MainPage from "./pages/MainPage/MainPage";
-import Login from "./pages/Login/Login";
-import Logout from "./pages/Logout/Logout";
 
 import InitApp from "./pages/InitApp/InitApp";
 
@@ -14,11 +11,7 @@ function App() {
   return (
     <div className="rtl" style={{ minHeight: "100vh", height: "100vh" }}>
       <Routes>
-        <Route path="dashboard/*" element={<Dashboard />} />
-        <Route path="/logout" element={<Logout />} />
-        <Route path="/*" element={<Login />} />
-        {/* <Route path="/" element={<MainPage />} /> */}
-        {/* <Route path="/login" element={<Login />} /> */}
+        <Route path="/" element={<MainPage />} />
       </Routes>
       <InitApp />
     </div>
--- src/redux/degrees/degrees.selectors.js
+++
@@ -1,8 +0,0 @@
-import { factorySelector } from "../../redux/global/global.selectors";
-
-export const selectorDegreeItems = factorySelector({
-  baseId: "degrees_main.items",
-});
-export const degreeMainCheckItem = factorySelector({
-  baseId: "degrees_main.checkItem",
-});
